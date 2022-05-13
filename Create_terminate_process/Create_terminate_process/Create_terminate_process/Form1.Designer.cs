5d6c31daf9149e9ff84412110aa72f4ca02bbe52
SarcevicLuka <93555102+SarcevicLuka@users.noreply.github.com>
2022-05-13 22:07:24 +0200

    Add files via upload

---
+++ Create_terminate_process/Create_terminate_process/Create_terminate_process/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Create_terminate_process
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_start_first_process = new System.Windows.Forms.Button();
+            this.b_start_second_process = new System.Windows.Forms.Button();
+            this.b_start_third_process = new System.Windows.Forms.Button();
+            this.lbl_first_process = new System.Windows.Forms.Label();
+            this.lbl_second_process = new System.Windows.Forms.Label();
+            this.lbl_third_process = new System.Windows.Forms.Label();
+            this.tb_first_process = new System.Windows.Forms.TextBox();
+            this.tb_second_process = new System.Windows.Forms.TextBox();
+            this.tb_third_process = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // b_start_first_process
+            // 
+            this.b_start_first_process.Location = new System.Drawing.Point(49, 224);
+            this.b_start_first_process.Name = "b_start_first_process";
+            this.b_start_first_process.Size = new System.Drawing.Size(157, 31);
+            this.b_start_first_process.TabIndex = 0;
+            this.b_start_first_process.Text = "Start first process";
+            this.b_start_first_process.UseVisualStyleBackColor = true;
+            this.b_start_first_process.Click += new System.EventHandler(this.b_start_first_process_Click);
+            // 
+            // b_start_second_process
+            // 
+            this.b_start_second_process.Location = new System.Drawing.Point(285, 224);
+            this.b_start_second_process.Name = "b_start_second_process";
+            this.b_start_second_process.Size = new System.Drawing.Size(157, 31);
+            this.b_start_second_process.TabIndex = 1;
+            this.b_start_second_process.Text = "Start second process";
+            this.b_start_second_process.UseVisualStyleBackColor = true;
+            this.b_start_second_process.Click += new System.EventHandler(this.b_start_second_process_Click);
+            // 
+            // b_start_third_process
+            // 
+            this.b_start_third_process.Location = new System.Drawing.Point(546, 224);
+            this.b_start_third_process.Name = "b_start_third_process";
+            this.b_start_third_process.Size = new System.Drawing.Size(157, 31);
+            this.b_start_third_process.TabIndex = 2;
+            this.b_start_third_process.Text = "Start third process";
+            this.b_start_third_process.UseVisualStyleBackColor = true;
+            this.b_start_third_process.Click += new System.EventHandler(this.b_start_third_process_Click);
+            // 
+            // lbl_first_process
+            // 
+            this.lbl_first_process.AutoSize = true;
+            this.lbl_first_process.Location = new System.Drawing.Point(46, 162);
+            this.lbl_first_process.Name = "lbl_first_process";
+            this.lbl_first_process.Size = new System.Drawing.Size(205, 17);
+            this.lbl_first_process.TabIndex = 3;
+            this.lbl_first_process.Text = "Waiting for first process to start";
+            // 
+            // lbl_second_process
+            // 
+            this.lbl_second_process.AutoSize = true;
+            this.lbl_second_process.Location = new System.Drawing.Point(282, 162);
+            this.lbl_second_process.Name = "lbl_second_process";
+            this.lbl_second_process.Size = new System.Drawing.Size(228, 17);
+            this.lbl_second_process.TabIndex = 4;
+            this.lbl_second_process.Text = "Waiting for second process to start";
+            // 
+            // lbl_third_process
+            // 
+            this.lbl_third_process.AutoSize = true;
+            this.lbl_third_process.Location = new System.Drawing.Point(543, 162);
+            this.lbl_third_process.Name = "lbl_third_process";
+            this.lbl_third_process.Size = new System.Drawing.Size(210, 17);
+            this.lbl_third_process.TabIndex = 5;
+            this.lbl_third_process.Text = "Waiting for third process to start";
+            // 
+            // tb_first_process
+            // 
+            this.tb_first_process.Location = new System.Drawing.Point(49, 102);
+            this.tb_first_process.Name = "tb_first_process";
+            this.tb_first_process.Size = new System.Drawing.Size(199, 22);
+            this.tb_first_process.TabIndex = 9;
+            this.tb_first_process.Text = "Enter first process to start";
+            this.tb_first_process.TextChanged += new System.EventHandler(this.tb_first_process_TextChanged);
+            // 
+            // tb_second_process
+            // 
+            this.tb_second_process.Location = new System.Drawing.Point(285, 102);
+            this.tb_second_process.Name = "tb_second_process";
+            this.tb_second_process.Size = new System.Drawing.Size(199, 22);
+            this.tb_second_process.TabIndex = 10;
+            this.tb_second_process.Text = "Enter second process to start";
+            this.tb_second_process.TextChanged += new System.EventHandler(this.tb_second_process_TextChanged);
+            // 
+            // tb_third_process
+            // 
+            this.tb_third_process.Location = new System.Drawing.Point(546, 102);
+            this.tb_third_process.Name = "tb_third_process";
+            this.tb_third_process.Size = new System.Drawing.Size(199, 22);
+            this.tb_third_process.TabIndex = 11;
+            this.tb_third_process.Text = "Enter third process to start";
+            this.tb_third_process.TextChanged += new System.EventHandler(this.tb_third_process_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(845, 348);
+            this.Controls.Add(this.tb_third_process);
+            this.Controls.Add(this.tb_second_process);
+            this.Controls.Add(this.tb_first_process);
+            this.Controls.Add(this.lbl_third_process);
+            this.Controls.Add(this.lbl_second_process);
+            this.Controls.Add(this.lbl_first_process);
+            this.Controls.Add(this.b_start_third_process);
+            this.Controls.Add(this.b_start_second_process);
+            this.Controls.Add(this.b_start_first_process);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_start_first_process;
+        private System.Windows.Forms.Button b_start_second_process;
+        private System.Windows.Forms.Button b_start_third_process;
+        private System.Windows.Forms.Label lbl_first_process;
+        private System.Windows.Forms.Label lbl_second_process;
+        private System.Windows.Forms.Label lbl_third_process;
+        private System.Windows.Forms.TextBox tb_first_process;
+        private System.Windows.Forms.TextBox tb_second_process;
+        private System.Windows.Forms.TextBox tb_third_process;
+    }
+}
+
---
+++ Create_terminate_process/Create_terminate_process/Create_terminate_process/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Create_terminate_process
+{
+    public partial class Form1 : Form
+    {
+
+        private string firstProcessName;
+        private string secondProcessName;
+        private string thirdProcessName;
+        private Thread tFirstProcess;
+        private Thread tSecondProcess;
+        private Thread tThirdProcess;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void b_start_first_process_Click(object sender, EventArgs e)
+         {
+            tFirstProcess = new Thread(() => StartFirstProcess());
+            tFirstProcess.Start();
+        }
+
+        private void b_start_second_process_Click(object sender, EventArgs e)
+        {
+            tSecondProcess = new Thread(() => StartSecondProcess());
+            tSecondProcess.Start();
+        }
+
+        private void b_start_third_process_Click(object sender, EventArgs e)
+        {
+            tThirdProcess = new Thread(() => StartThirdProcess());
+            tThirdProcess.Start();
+        }
+
+        private void StartFirstProcess() {
+            Process firstProcess = new Process();
+            firstProcess.StartInfo.FileName = this.firstProcessName;
+            firstProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            firstProcess.Start();
+
+            this.Invoke(new Action(() => { b_start_first_process.Enabled = false; }));
+            this.Invoke(new Action(() => { lbl_first_process.Text = firstProcessName + " has been started!"; }));
+            firstProcess.WaitForExit();
+            this.Invoke(new Action(() => { b_start_first_process.Enabled = true; }));
+            this.Invoke(new Action(() => { lbl_first_process.Text = firstProcessName + " has been terminated!"; }));
+
+            tFirstProcess.Abort();
+        }
+
+        private void StartSecondProcess()
+        {
+            Process secondProcess = new Process();
+            secondProcess.StartInfo.FileName = this.secondProcessName;
+            secondProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            secondProcess.Start();
+
+            this.Invoke(new Action(() => { b_start_second_process.Enabled = false; }));
+            this.Invoke(new Action(() => { lbl_second_process.Text = secondProcessName + " has been started!"; }));
+            secondProcess.WaitForExit();
+            this.Invoke(new Action(() => { b_start_second_process.Enabled = true; }));
+            this.Invoke(new Action(() => { lbl_second_process.Text = secondProcessName + " has been terminated!"; }));
+
+            tSecondProcess.Abort();
+        }
+
+        private void StartThirdProcess()
+        {
+            Process thirdProcess = new Process();
+            thirdProcess.StartInfo.FileName = this.thirdProcessName;
+            thirdProcess.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            thirdProcess.Start();
+
+            this.Invoke(new Action(() => { b_start_third_process.Enabled = false; }));
+            this.Invoke(new Action(() => { lbl_third_process.Text = thirdProcessName + " has been started!"; }));
+            thirdProcess.WaitForExit();
+            this.Invoke(new Action(() => { b_start_third_process.Enabled = true; }));
+            this.Invoke(new Action(() => { lbl_third_process.Text = thirdProcessName + " has been terminated!"; }));
+
+            tThirdProcess.Abort();
+        }
+
+        private void tb_first_process_TextChanged(object sender, EventArgs e)
+        {
+            this.firstProcessName = tb_first_process.Text;
+            if (firstProcessName.Split('.').Last() != "exe") {
+                firstProcessName += ".exe";
+            }
+        }
+
+        private void tb_second_process_TextChanged(object sender, EventArgs e)
+        {
+            this.secondProcessName = tb_second_process.Text;
+            if (secondProcessName.Split('.').Last() != "exe")
+            {
+                secondProcessName += ".exe";
+            }
+        }
+
+        private void tb_third_process_TextChanged(object sender, EventArgs e)
+        {
+            this.thirdProcessName = tb_third_process.Text;
+            if (thirdProcessName.Split('.').Last() != "exe")
+            {
+                thirdProcessName += ".exe";
+            }
+        }
+    }
+}
